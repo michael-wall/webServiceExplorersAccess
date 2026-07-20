f8d6048e31d792092aee34bcac2e36ed30a4b106
Michael Wall <Michael Wall@host.docker.internal>
2026-07-20 12:24:40 +0100

    cleanup

--- modules/WebServiceExplorersAccess/src/main/java/com/mw/WebServiceExplorerFilter.java
+++ modules/WebServiceExplorersAccess/src/main/java/com/mw/WebServiceExplorerFilter.java
@@ -21,13 +21,13 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
     property = {
     	"servlet-context-name=", 
-    	"servlet-filter-name=Headless Explorer Filter",
+    	"servlet-filter-name=Web Service Explorer Filter",
         "url-pattern=/o/api/*",
         "url-pattern=/api/jsonws/*"
     },
     service = Filter.class
 )
-public class APIExplorerServletFilter extends BaseFilter {
+public class WebServiceExplorerFilter extends BaseFilter {
 	
 	@Activate
 	protected void activate(Map<String, Object> properties)  throws Exception {
@@ -45,21 +45,24 @@ public class APIExplorerServletFilter extends BaseFilter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         
         boolean restrictedHeadlessEndpoint = false;
+        boolean allowedHeadlessEndpoint = false;
         boolean restrictedJsonwsEndpoint = false;
         
         _log.info("requestURI: " + request.getRequestURI());
         _log.info("queryString: " + request.getQueryString());
         
-        if (request.getRequestURI().toLowerCase().startsWith("/o/api")) { // Explorer called
+        if (request.getRequestURI().toLowerCase().startsWith("/o/api/main.css")) { // Explorer CSS file called
+        	allowedHeadlessEndpoint = true;
+        } else if (request.getRequestURI().toLowerCase().startsWith("/o/api")) { // Explorer called
         	restrictedHeadlessEndpoint = true;
         }
         
-        if (!restrictedHeadlessEndpoint) {
+        if (!allowedHeadlessEndpoint && !restrictedHeadlessEndpoint) {
             // Means they are accessing the Explorer
         	// We must not block /api/jsonws/audit.auditevent/get-audit-events-count for example...
             if (request.getRequestURI().equalsIgnoreCase("/api/jsonws") || request.getRequestURI().equalsIgnoreCase("/api/jsonws/")) {
             	restrictedJsonwsEndpoint = true;
-            } else if (request.getQueryString().toLowerCase().contains("contentPath".toLowerCase())) {
+            } else if (request.getQueryString() != null && request.getQueryString().toLowerCase().contains("contentPath".toLowerCase())) {
             	restrictedJsonwsEndpoint = true;
             }
         }
@@ -68,10 +71,13 @@ public class APIExplorerServletFilter extends BaseFilter {
             long userId = PortalUtil.getUserId(httpRequest);
             User user = UserLocalServiceUtil.fetchUser(userId);
         	
-            if (user == null || !user.getScreenName().equalsIgnoreCase("test")) {
-            	_log.info("Blocking...");
+            if (user == null || !user.getScreenName().equalsIgnoreCase("test")) { // TODO Replace with custom logic...
+            	if (user == null) {
+            		_log.info("Blocking...");	
+            	} else {
+            		_log.info("Blocking " + user.getScreenName() + " ...");
+            	}
             	
-            	//new ServletException("Blocked");	
             	response.sendError(HttpServletResponse.SC_FORBIDDEN);
             	return;
             } else {
@@ -80,7 +86,9 @@ public class APIExplorerServletFilter extends BaseFilter {
             	filterChain.doFilter(request, response);  
             }	
         } else {
-        	_log.info("Bypassing Access check...");
+        	if (!allowedHeadlessEndpoint) {
+        		_log.info("Bypassing Access check...");	
+        	}
         	
         	filterChain.doFilter(request, response);        	
         }
@@ -91,5 +99,5 @@ public class APIExplorerServletFilter extends BaseFilter {
 		return _log;
 	}
 	
-	private static final Log _log = LogFactoryUtil.getLog(APIExplorerServletFilter.class);
+	private static final Log _log = LogFactoryUtil.getLog(WebServiceExplorerFilter.class);
 }
